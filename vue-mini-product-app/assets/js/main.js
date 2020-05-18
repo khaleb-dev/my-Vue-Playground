d3f72b43e6c040fa5a527f6d73abc31776cff2ea
Khaleb O'Brien <ebukauche52@gmail.com>
2020-05-18 12:29:16 +0100

    previous snippets

---
+++ vue-mini-product-app/assets/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		product: "Socks",
+		image: "./assets/img/greenSocks.jpg",
+		altText: "A pair of Socks",
+		description: "Socks helps keep our feet warm."
+	}
+})
