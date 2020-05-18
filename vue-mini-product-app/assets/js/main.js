1ecdafade7ab981f9d25310f564824a84b0ea23d
Khaleb O'Brien <ebukauche52@gmail.com>
2020-05-18 17:07:53 +0100

    fiddled with computed properties, style,class and attribute binding, conditional and list rendering, event handling...

--- vue-mini-product-app/assets/js/main.js
+++ vue-mini-product-app/assets/js/main.js
@@ -1,9 +1,38 @@
 var app = new Vue({
 	el: "#app",
 	data: {
+		brand: "Vue",
 		product: "Socks",
 		image: "./assets/img/greenSocks.jpg",
 		altText: "A pair of Socks",
-		description: "Socks helps keep our feet warm."
+		inStock: true,
+		inventory: 52,
+		details: ["85% Cotton", "15% Polyster", "Gender-neutral"],
+		variants: [
+			{
+				variantId: 2245,
+				variantColor: "blue",
+				variantImage: "./assets/img/blueSocks.jpg",
+			},
+			{
+				variantId: 2253,
+				variantColor: "green",
+				variantImage: "./assets/img/greenSocks.jpg",
+			}
+		],
+		cart: 0,
+	},
+	methods: {
+		addToCart: function () {
+			this.cart += 1
+		},
+		updateProduct: function (variantImage) {
+			this.image = variantImage
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product
+		}
 	}
 })
